9167a3ae1cafad3f9d900277b3df8c4d30e9072b
nullvalley <lwright0106@gmail.com>
2022-04-14 15:17:05 -0500

    Test
    Testing stuff

--- CoffeeShopCIS/CoffeeShopC-I-S/CoffeeShopC-I-S/Form1.Designer.cs
+++ CoffeeShopCIS/CoffeeShopC-I-S/CoffeeShopC-I-S/Form1.Designer.cs
@@ -29,13 +29,33 @@ namespace CoffeeShopC_I_S
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(387, 139);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "F***";
+            this.testButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button testButton;
     }
 }
 
