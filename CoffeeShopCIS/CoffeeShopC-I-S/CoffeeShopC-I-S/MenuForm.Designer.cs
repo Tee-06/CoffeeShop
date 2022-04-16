c84a7798e349494c12bdad95b898652a247ed23f
Tyler <tylerpetersenbusiness@gmail.com>
2022-04-15 20:37:31 -0500

    Whatever these are
    /* these are just on load*/

--- CoffeeShopCIS/CoffeeShopC-I-S/CoffeeShopC-I-S/MenuForm.Designer.cs
+++ CoffeeShopCIS/CoffeeShopC-I-S/CoffeeShopC-I-S/MenuForm.Designer.cs
@@ -179,7 +179,6 @@ namespace CoffeeShopC_I_S
             this.ownerMenuBtn.TabIndex = 19;
             this.ownerMenuBtn.Text = "Owner\'s Menu";
             this.ownerMenuBtn.UseVisualStyleBackColor = false;
-            this.ownerMenuBtn.Click += new System.EventHandler(this.ownerMenuBtn_Click);
             // 
             // removeBtn
             // 
